91045ea96e9731ccb532b398ff24c142e02f4981
George <megageorge93@mail.ru>
2019-03-23 21:05:58 +0400

    Cards Activity: Added insert folder function
    Deleted fake inserts

--- app/src/main/java/com/example/smartcardsnew/MyCardsActivity.java
+++ app/src/main/java/com/example/smartcardsnew/MyCardsActivity.java
@@ -1,9 +1,12 @@
 package com.example.smartcardsnew;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.smartcardsnew.Adapters.FoldersRecyclerAdapter;
 import com.example.smartcardsnew.Models.Folders;
@@ -16,6 +19,7 @@ public class MyCardsActivity extends AppCompatActivity {
 
    private ArrayList<Folders> mFolders = new ArrayList<>();
    private FoldersRecyclerAdapter mFoldersRecyclerAdapter;
+   private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +27,24 @@ public class MyCardsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_cards);
         mRecyclerView = findViewById(R.id.folder_RecyclerView);
 
+        //adding new Folder
+        addButton = findViewById(R.id.fab);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFolder();
+            }
+        });
+
         initRecyclerView();
-        insertFolders();
+
     }
 
-    private void insertFolders(){
-        for(int i = 0; i<100; i++){
-        Folders folder = new Folders();
-        folder.setFolderName("title" + i);
 
-        mFolders.add(folder);}
+    private void addFolder(){
+        Folders newFolder = new Folders();
+        newFolder.setFolderName("New Folder");
+        mFolders.add(newFolder);
         mFoldersRecyclerAdapter.notifyDataSetChanged();
     }
 private void initRecyclerView(){
